6c7b281254a2ad76198426f86f1c15d3e987e121
Andy Loredo <ndy.loredo@gmail.com>
2023-07-05 20:41:34 -0400

    add front end files and styled home page background image for banner needs fixing

---
+++ db/migrate/20230705233258_create_jobs.rb
@@ -0,0 +1,11 @@
+class CreateJobs < ActiveRecord::Migration[7.0]
+  def change
+    create_table :jobs do |t|
+      t.string :name
+      t.text :description
+      t.decimal :price
+
+      t.timestamps
+    end
+  end
+end
